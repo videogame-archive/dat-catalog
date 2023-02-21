e814f90ff35e5c7fae9effa686d4cfe89c13dba3
optyfr <17027109+optyfr@users.noreply.github.com>
2023-02-21 09:17:51 +0100

    restructure for helpers and checkers

--- .build.sh
+++ .build.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
+###################
+### DAT CATALOG ###
+###################
 
+## 0 : global vars
 DIR="$(dirname -- "$(readlink -f -- "$0")")"
+ROOT="$DIR/root"
+BUILD="$DIR/.build"
 
-pushd "$DIR/.build" >/dev/null
-
+## 1 : check requirements
+pushd "$BUILD/checkers" >/dev/null
 IFS=$'\n'
-for f in *.sh
-do
-	echo "Processing $f file..."
-	. "$f"
-done
-
+for f in *.sh; do . "$f"; done
 popd >/dev/null
+
+## 2 : load helpers
+pushd "$BUILD/helpers" >/dev/null
+IFS=$'\n'
+for f in *.sh; do . "$f"; done
+popd >/dev/null
+
+## 3 : build collections 
+pushd "$BUILD/builders" >/dev/null
+IFS=$'\n'
+for f in *.sh; do . "$f"; done
+popd >/dev/null
+
