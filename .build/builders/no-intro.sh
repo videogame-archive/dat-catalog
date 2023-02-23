e348eeb1f5127bf8957914c8db6d4efa5c5934b2
optyfr <17027109+optyfr@users.noreply.github.com>
2023-02-23 10:52:20 +0100

    no-intro done

---
+++ .build/builders/no-intro.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+###############################
+### No-Intro Collection ###
+###############################
+
+NI_DIR="$ROOT/No-Intro"
+NI_URL="https://datomatic.no-intro.org/"
+
+# tell datomatic to download a specific archive type
+NI_PAGE="index.php?page=download&op=daily&s=64"
+
+# tell what to include in the archive
+NI_FORM="dat_type=standard&set1=Ok&valentine_day=Request"
+
+NI_COOKIE="/tmp/ni_cookie.txt"
+NI_ARCHIVE="/tmp/ni_archive.zip"
+
+white "\n*** Building No-Intro collection ***\n"
+mkdir -p "$NI_DIR"
+
+pushd "$NI_DIR" >/dev/null
+
+if [ "$(get_current_date)" -gt "$(get_modified_date)" ]
+then
+	dot_green
+	echo -n " unzipping global archive..."
+	location=$(curl -s -i -H "Content-Type: application/x-www-form-urlencoded" -d "${NI_FORM}" -c "${NI_COOKIE}" "${NI_URL}${NI_PAGE}" | grep -oP 'location: \K.*' | tr -d '[:space:]')
+	curl -s -S -H "Content-Type: application/x-www-form-urlencoded" -d "lazy_mode=Download" -b "${NI_COOKIE}" -o "${NI_ARCHIVE}" "${NI_URL}${location}"
+	rm "${NI_COOKIE}"
+	
+	if [ -f "${NI_ARCHIVE}" ];
+	then
+		DONE
+		zipinfo -t "${NI_ARCHIVE}" >/dev/null 2>&1
+		if [ $? -eq 0 ];
+		then
+			dot_green
+			echo -n " unzipping global archive..."
+			rm -rf ./*
+			unzip -q "${NI_ARCHIVE}"
+			rm *.txt
+			rm "${NI_ARCHIVE}"
+			write_modified
+			DONE
+		else
+			dot_red
+			echo -n " failed to unzip archive... please look at ${NI_ARCHIVE} content}"
+			BAD
+		fi
+	else
+		BAD
+	fi
+else
+	dot_yellow
+	echo -n " Already downloaded today"
+	MISS
+fi
+
+popd >/dev/null
