8ce0fb50c40a3d0ce1420d40c4ec57961b7c6f7e
JuMaFuSe <open-retrogaming-archive@protonmai.com>
2023-02-25 21:54:36 +0100

    ongoing work

--- Build/src/main/java/com/github/openretrogamingarchive/TOSECUpdater.java
+++ Build/src/main/java/com/github/openretrogamingarchive/TOSECUpdater.java
@@ -8,10 +8,20 @@ import static com.github.openretrogamingarchive.Util.downloadBytes;
 import static com.github.openretrogamingarchive.Util.scrap;
 
 public class TOSECUpdater {
-    private static final String DOWNLOAD_URL = "https://www.tosecdev.org/downloads";
+
+    public static final String DOMAIN = "https://www.tosecdev.org";
+
+    private static final String DOWNLOADS_URL = DOMAIN + "/downloads";
     public static void updateTOSEC(String pathToRoot) throws IOException {
-        String publicDownloadsPage = new String(downloadBytes(DOWNLOAD_URL), StandardCharsets.UTF_8);
+        byte[] lastReleaseZip = getLastRelease();
+        int breakpoint = 0;
+    }
+
+    private static byte[] getLastRelease() throws IOException {
+        String publicDownloadsPage = new String(downloadBytes(DOWNLOADS_URL), StandardCharsets.UTF_8);
         String releaseTable = scrap(publicDownloadsPage, "<div class=\"pd-categoriesbox\">", "<div class=\"pd-categoriesbox\">").get(0);
         List<String> releases = scrap(releaseTable, "<a href=\"", "\">");
+        String lastReleaseURL = DOMAIN + releases.get(1);
+        return Util.downloadBytes(lastReleaseURL);
     }
 }
