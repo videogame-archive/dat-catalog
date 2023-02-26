cf47280bf42fcdfb33812eda2efd533b5d96195a
Juan Fuentes <open-retrogaming-archive@protonmai.com>
2023-02-26 14:49:18 +0100

    ongoing work - redump refactoring + basic dir links

--- Build/src/main/java/com/github/openretrogamingarchive/Main.java
+++ Build/src/main/java/com/github/openretrogamingarchive/Main.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 
 public class Main {
 
-    private static final String ROOT_LATEST_DIR = "../root/normalized";
+    private static final String ROOT_LATEST_DIR = "../root/";
 
     public static void main(String[] args) throws IOException {
         RedumpUpdater.updateRedump(ROOT_LATEST_DIR);
-        TOSECUpdater.updateTOSEC(ROOT_LATEST_DIR);
+        //TOSECUpdater.updateTOSEC(ROOT_LATEST_DIR);
     }
 }
