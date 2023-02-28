9b92af9e79badd8f8c774e2af33128d4e2c746d4
optyfr <17027109+optyfr@users.noreply.github.com>
2023-02-28 23:24:03 +0100

    fix bugs

--- Build/src/main/java/com/github/openretrogamingarchive/helpers/HTTP.java
+++ Build/src/main/java/com/github/openretrogamingarchive/helpers/HTTP.java
@@ -18,92 +18,90 @@ import java.util.Optional;
 import java.util.regex.Pattern;
 
 public class HTTP {
-    private HttpClient client;
+	private HttpClient client;
 
-    public HTTP() {
-	final var builder = HttpClient.newBuilder();
-	builder.followRedirects(HttpClient.Redirect.NORMAL);
-	builder.cookieHandler(new CookieManager(null, CookiePolicy.ACCEPT_ALL));
-	client = builder.build();
-    }
-
-    public HttpResponse<String> postFormExpectString(URI uri, String formdata)
-	    throws IOException, InterruptedException {
-	final var req = HttpRequest.newBuilder().uri(uri).headers("Content-Type", "application/x-www-form-urlencoded")
-		.POST(BodyPublishers.ofString(formdata)).build();
-	return client.send(req, HttpResponse.BodyHandlers.ofString());
-    }
-
-    public HttpResponse<InputStream> postFormExpectInputStream(URI uri, String formdata)
-	    throws IOException, InterruptedException {
-	final var req = HttpRequest.newBuilder().uri(uri).headers("Content-Type", "application/x-www-form-urlencoded")
-		.POST(BodyPublishers.ofString(formdata)).build();
-	return client.send(req, HttpResponse.BodyHandlers.ofInputStream());
-    }
-
-    public HttpResponse<InputStream> getExpectInputStream(URI uri) throws IOException, InterruptedException {
-	final var req = HttpRequest.newBuilder().uri(uri).GET().build();
-	return client.send(req, HttpResponse.BodyHandlers.ofInputStream());
-    }
-
-    public HttpResponse<String> getExpectString(URI uri) throws IOException, InterruptedException {
-	final var req = HttpRequest.newBuilder().uri(uri).GET().build();
-	return client.send(req, HttpResponse.BodyHandlers.ofString());
-    }
-
-    public static final boolean hasZip(HttpHeaders headers) {
-	return headers.firstValue("content-type").map(v -> v.equals("application/zip")).orElse(false);
-    }
-
-    private static final Pattern contentDisposition = Pattern.compile("filename=\"(.*?)\"");
-
-    public static final Optional<String> extractFilename(HttpHeaders header) {
-	return header.firstValue("content-disposition").map(value -> {
-	    final var matcher = contentDisposition.matcher(value);
-	    if (matcher.matches() && matcher.groupCount() == 1) {
-		return matcher.group(1);
-	    }
-	    return null;
-	});
-    }
-
-    public static final void writeToFile(HttpResponse<InputStream> resp, Path file) throws IOException {
-	try (final var out = Files.newOutputStream(file)) {
-	    resp.body().transferTo(out);
+	public HTTP() {
+		final var builder = HttpClient.newBuilder();
+		builder.followRedirects(HttpClient.Redirect.NORMAL);
+		builder.cookieHandler(new CookieManager(null, CookiePolicy.ACCEPT_ALL));
+		client = builder.build();
 	}
-    }
 
-    public static String downloadAsString(String uri) throws IOException, InterruptedException, URISyntaxException
-    {
-	return new HTTP().getExpectString(new URI(uri)).body();
-    }
-    
-    public static InputStream downloadAsStream(String uri) throws IOException, InterruptedException, URISyntaxException
-    {
-	return new HTTP().getExpectInputStream(new URI(uri)).body();
-    }
+	public HttpResponse<String> postFormExpectString(URI uri, String formdata)
+			throws IOException, InterruptedException {
+		final var req = HttpRequest.newBuilder().uri(uri).headers("Content-Type", "application/x-www-form-urlencoded")
+				.POST(BodyPublishers.ofString(formdata)).build();
+		return client.send(req, HttpResponse.BodyHandlers.ofString());
+	}
 
-    public static Path downloadToFolder(URI uri, Path parent, boolean unZip)
-	    throws IOException, InterruptedException {
+	public HttpResponse<InputStream> postFormExpectInputStream(URI uri, String formdata)
+			throws IOException, InterruptedException {
+		final var req = HttpRequest.newBuilder().uri(uri).headers("Content-Type", "application/x-www-form-urlencoded")
+				.POST(BodyPublishers.ofString(formdata)).build();
+		return client.send(req, HttpResponse.BodyHandlers.ofInputStream());
+	}
 
-	final var download = new HTTP().getExpectInputStream(uri);
-	final var filename = extractFilename(download.headers());
+	public HttpResponse<InputStream> getExpectInputStream(URI uri) throws IOException, InterruptedException {
+		final var req = HttpRequest.newBuilder().uri(uri).GET().build();
+		return client.send(req, HttpResponse.BodyHandlers.ofInputStream());
+	}
 
-	if (unZip && hasZip(download.headers())) {
-	    try (final var zip = new ZIP(download.body(), filename)) {
-		zip.extractAll(parent);
-	    }
-	    return parent;
-	} else {
-	    if (filename.isPresent()) {
-		Path file = parent.resolve(filename.get());
-		try (final var out = new BufferedOutputStream(Files.newOutputStream(parent))) {
-		    download.body().transferTo(out);
+	public HttpResponse<String> getExpectString(URI uri) throws IOException, InterruptedException {
+		final var req = HttpRequest.newBuilder().uri(uri).GET().build();
+		return client.send(req, HttpResponse.BodyHandlers.ofString());
+	}
+
+	public static final boolean hasZip(HttpHeaders headers) {
+		return headers.firstValue("content-type").map(v -> v.equals("application/zip")).orElse(false);
+	}
+
+	private static final Pattern contentDisposition = Pattern.compile("filename=\"(.*?)\"");
+
+	public static final Optional<String> extractFilename(HttpHeaders header) {
+		return header.firstValue("content-disposition").map(value -> {
+			final var matcher = contentDisposition.matcher(value);
+			if (matcher.find() && matcher.groupCount() == 1) {
+				return matcher.group(1);
+			}
+			return null;
+		});
+	}
+
+	public static final void writeToFile(HttpResponse<InputStream> resp, Path file) throws IOException {
+		try (final var out = Files.newOutputStream(file)) {
+			resp.body().transferTo(out);
 		}
-		return file;
-	    }
 	}
-	return null;
-    }
+
+	public static String downloadAsString(String uri) throws IOException, InterruptedException, URISyntaxException {
+		return new HTTP().getExpectString(new URI(uri)).body();
+	}
+
+	public static InputStream downloadAsStream(String uri)
+			throws IOException, InterruptedException, URISyntaxException {
+		return new HTTP().getExpectInputStream(new URI(uri)).body();
+	}
+
+	public static Path downloadToFolder(URI uri, Path parent, boolean unZip) throws IOException, InterruptedException {
+
+		final var download = new HTTP().getExpectInputStream(uri);
+		final var filename = extractFilename(download.headers());
+
+		if (unZip && hasZip(download.headers())) {
+			try (final var zip = new ZIP(download.body(), filename)) {
+				zip.extractAll(parent);
+			}
+			return parent;
+		} else {
+			if (filename.isPresent()) {
+				Path file = parent.resolve(filename.get());
+				try (final var out = new BufferedOutputStream(Files.newOutputStream(file))) {
+					download.body().transferTo(out);
+				}
+				return file;
+			}
+		}
+		return null;
+	}
 
 }
