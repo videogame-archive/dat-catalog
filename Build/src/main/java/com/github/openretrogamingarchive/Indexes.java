175c6cd1f3f69f9871635ac7b5d81526f33d654b
optyfr <17027109+optyfr@users.noreply.github.com>
2023-03-04 22:14:51 +0100

    Fixes

--- Build/src/main/java/com/github/openretrogamingarchive/Indexes.java
+++ Build/src/main/java/com/github/openretrogamingarchive/Indexes.java
@@ -17,19 +17,21 @@ public class Indexes extends UpdaterBase {
 	
 	static void update(Path current) throws IOException {
 		List<String[]> currentIndex = new ArrayList<>();
-		Files.list(current).filter(s -> !s.getFileName().toString().startsWith(".")).forEach(fileInDir -> {
+		try (final var stream = Files.list(current)) {
+		    stream.filter(s -> !s.getFileName().toString().startsWith(".")).forEach(fileInDir -> {
 			try {
-				URL url = null;
-				if (Files.isDirectory(fileInDir)) {
-					update(fileInDir);
-				} else {
-					url = new URL(BASE_URL + BASE_DIR.relativize(fileInDir));
-				}
-				currentIndex.add(CSV.toRow(fileInDir, url));
+			    URL url = null;
+			    if (Files.isDirectory(fileInDir)) {
+				update(fileInDir);
+			    } else {
+				url = new URL(BASE_URL + BASE_DIR.relativize(fileInDir));
+			    }
+			    currentIndex.add(CSV.toRow(fileInDir, url));
 			} catch (IOException e) {
-				e.printStackTrace();
+			    e.printStackTrace();
 			}
-		});
+		    });
+		}
 		CSV.save(current, currentIndex);
 	}
 
