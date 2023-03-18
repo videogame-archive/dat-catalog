1bb8e327ef46820daca74267c4219ccfcee99157
optyfr <17027109+optyfr@users.noreply.github.com>
2023-03-18 14:37:58 +0100

    conform to sonar recommendations

--- build/src/main/java/com/github/openretrogamingarchive/updaters/PD.java
+++ build/src/main/java/com/github/openretrogamingarchive/updaters/PD.java
@@ -5,10 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -22,8 +21,8 @@ public class PD extends Updater {
 	private static final String PD_LINE_REG = "Datfile.*:.*\\(https://.*.zip\\).*";
 	private static final String MD_LINK_REG = "\\[(.*)\\].*\\((https:.*.zip)\\)";
 
-    private final Pattern PD_LINE_PATTERN = Pattern.compile(PD_LINE_REG); 
-    private final Pattern MD_LINK_PATTERN = Pattern.compile(MD_LINK_REG); 
+    private static final Pattern PD_LINE_PATTERN = Pattern.compile(PD_LINE_REG); 
+    private static final Pattern MD_LINK_PATTERN = Pattern.compile(MD_LINK_REG); 
     
     @Override
 	public void update() throws Exception {
@@ -42,23 +41,18 @@ public class PD extends Updater {
 
 	private void update(String name, String url) throws IOException, InterruptedException, URISyntaxException
 	{
-		final var PD_REF_ROOT = NORMALIZED_DIR.resolve(PD_DIR).resolve(name);
+		final var refRoot = NORMALIZED_DIR.resolve(PD_DIR).resolve(name);
 		
 		try(final var in = new BufferedReader(new InputStreamReader(HTTP.downloadAsStream(url))))
 		{
-			if(Files.exists(PD_REF_ROOT))
-				Util.deleteRecursive(PD_REF_ROOT);
-			Files.createDirectories(PD_REF_ROOT);
-			for(final var nf : in.lines().filter(l -> PD_LINE_PATTERN.matcher(l).matches()).map(l -> MD_LINK_PATTERN.matcher(l)).filter(m -> m.find()).collect(Collectors.toMap(k -> k.group(1), k -> k.group(2))).entrySet())
+			if(Files.exists(refRoot))
+				Util.deleteRecursive(refRoot);
+			Files.createDirectories(refRoot);
+			for(final var nf : in.lines().filter(l -> PD_LINE_PATTERN.matcher(l).matches()).map(l -> MD_LINK_PATTERN.matcher(l)).filter(Matcher::find).collect(Collectors.toMap(k -> k.group(1), k -> k.group(2))).entrySet())
 			{
 				final var encoded = nf.getValue().replace(" ", "%20");
-				HTTP.downloadToFolder(URI.create(encoded), PD_REF_ROOT, ZIPMode.SMART);
+				HTTP.downloadToFolder(URI.create(encoded), refRoot, ZIPMode.SMART);
 			}
-		}
-		catch(Exception ex)
-		{
-			ex.printStackTrace();
-			throw ex;
 		}
 	}
 	
